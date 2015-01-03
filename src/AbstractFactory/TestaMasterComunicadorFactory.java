79dc9186ce5d189c1ca6465ab5268423141d0f23
Alexsandro Facanha <asfsteel2012@gmail.com>
2015-01-03 00:21:24 -0300

    commit readme

--- src/AbstractFactory/TestaMasterComunicadorFactory.java
+++ src/AbstractFactory/TestaMasterComunicadorFactory.java
@@ -3,9 +3,15 @@ package AbstractFactory;
 public class TestaMasterComunicadorFactory {
 	
 	public static void main(String[] args) {
+		ComunicadorFactory cf = new MasterComunicadorFactory();
 		
 		String transacao = "Valor=1200;Sanha=321"; 
+		Emissor emissor = cf.createEmissor();
+		emissor.enviar(transacao);
 		
+		Receptor receptor = cf.createReceptor();
+		String mensagem = receptor.recebe();
+		System.out.println(mensagem);
 		
 	}
 }
